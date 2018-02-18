81be818f11899ef32c1a9d773a1f66f39a8ce358
Ryan <rysherrell@gmail.com>
2018-02-17 22:27:59 -0600

    updated dj-js to es6

--- exercises/dj-js/script.js
+++ exercises/dj-js/script.js
@@ -1,35 +1,35 @@
 var box = document.getElementById("box");
 
-box.addEventListener("mouseover", function(){box.style.backgroundColor = "blue"});
-document.addEventListener("keydown", function(e){
+box.addEventListener("mouseover", () => {box.style.backgroundColor = "blue"});
+document.addEventListener("keydown", e => {
     var x = e.keyCode;
     if (x == 66) {
         box.style.backgroundColor = "blue";
     }
 });
-box.addEventListener("mousedown", function(){box.style.backgroundColor = "red"});
-document.addEventListener("keydown", function(e){
+box.addEventListener("mousedown", () => {box.style.backgroundColor = "red"});
+document.addEventListener("keydown", e => {
     var x = e.keyCode;
     if (x == 82) {
         box.style.backgroundColor = "red";
     }
 });
-box.addEventListener("mouseup", function(){box.style.backgroundColor = "yellow"});
-document.addEventListener("keydown", function(e){
+box.addEventListener("mouseup", () => {box.style.backgroundColor = "yellow"});
+document.addEventListener("keydown", e => {
     var x = e.keyCode;
     if (x == 89) {
         box.style.backgroundColor = "yellow";
     }
 });
-box.addEventListener("dblclick", function(){box.style.backgroundColor = "green"});
-document.addEventListener("keydown", function(e){
+box.addEventListener("dblclick", () => {box.style.backgroundColor = "green"});
+document.addEventListener("keydown", e => {
     var x = e.keyCode;
     if (x == 71) {
         box.style.backgroundColor = "green";
     }
 });
-box.addEventListener("wheel", function(){box.style.backgroundColor = "orange"});
-document.addEventListener("keydown", function(e){
+box.addEventListener("wheel", () => {box.style.backgroundColor = "orange"});
+document.addEventListener("keydown", e => {
     var x = e.keyCode;
     if (x == 79) {
         box.style.backgroundColor = "orange";
