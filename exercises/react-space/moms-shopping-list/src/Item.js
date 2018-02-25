9ae6430286725953ec7f101157f1d6a0534ad40a
Ryan <rysherrell@gmail.com>
2018-02-25 16:02:30 -0700

    blog-props

--- exercises/react-space/moms-shopping-list/src/Item.js
+++ exercises/react-space/moms-shopping-list/src/Item.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Item(props) {
     return (
-        <li><button className="itemDoneButton" onClick={props.handleItemDone}>X</button><button className="itemDelButton" onClick={props.handleItemDelete}>X</button>{props.entry}</li>
+        <li>
+            <button className="itemDoneButton" onClick={props.handleItemDone}>
+                X
+            </button>
+            <button className="itemDelButton" onClick={props.handleItemDelete}>
+                X
+            </button>
+            {props.entry}
+        </li>
     )
 }
 
