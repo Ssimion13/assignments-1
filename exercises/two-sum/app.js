47ec482e45d28187f255fd307e91d67173bdfd74
Ryan <rysherrell@gmail.com>
2018-02-28 09:20:28 -0700

    two-sum

---
+++ exercises/two-sum/app.js
@@ -0,0 +1,15 @@
+const twoSum = (arr, int) => {
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === int) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+console.log(twoSum([1,2,3], 4));
+console.log(twoSum([1,2,3], 5));
+console.log(twoSum([1,2,3], 3));
+//returns [0, 2] because 1 + 3 equals 4
