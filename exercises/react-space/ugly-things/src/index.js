4be91c22c9bcb1fd27c6bc223413141648b3f637
Ryan <rysherrell@gmail.com>
2018-03-01 17:44:43 -0700

    ugly-things

---
+++ exercises/react-space/ugly-things/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'react';
+import { provider } from 'react-redux';
+
+import reducer from "./redux";
+
+import './index.css';
+import App from './App';
+
+const store = createStore(
+    reducer,
+    window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+);
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
