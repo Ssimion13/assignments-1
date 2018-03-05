8fd163bfb044dbcc3ed947fbe47248c7cfebd9eb
Ryan <rysherrell@gmail.com>
2018-03-05 11:38:29 -0700

    practice-space

--- practice-space/script.js
+++ practice-space/script.js
@@ -1,20 +1,52 @@
-function makeArrayConsecutive2(statues) {
-    let extrasArr = [];
-    let statuesArr = statues.sort((a, b) => a - b);
+function almostIncreasingSequence(sequence) {
 
-    let smallest = 21;
-    let largest = 0;
+    let sortedArr = sequence.sort((a, b) => {
+        return a - b;
+    })
 
-    for (let i = 0; i < statuesArr.length; i++) {
-        if (statues[i] > largest) {
-            largest = statues[i];
-        }
-        if (statues[i] < smallest) {
-            smallest = statues[i];
+    let count = 0;
+
+    for (let i = 0; i < sortedArr.length; i++) {
+        if (sortedArr[i] === sortedArr[i + 1]) {
+            count++;
         }
     }
-
-    return ((largest - smallest) + 1) - statues.length;
+    if (count > 1) {
+        return false;
+    }
+    for (let i = 0; i < sortedArr.length; i++) {
+        if (sortedArr[i] > sortedArr[i + 1]) {
+            count++;
+            console.log(count);
+        }
+    }
+    if (count < 2) {
+        return true;
+    }
+    return false;
 }
 
-console.log(makeArrayConsecutive2([5, 2, 8, 4, 14]));
+console.log(almostIncreasingSequence([1, 3, 2]));
+
+//###################################################################
+
+// function makeArrayConsecutive2(statues) {
+//     let extrasArr = [];
+//     let statuesArr = statues.sort((a, b) => a - b);
+//
+//     let smallest = 21;
+//     let largest = 0;
+//
+//     for (let i = 0; i < statuesArr.length; i++) {
+//         if (statues[i] > largest) {
+//             largest = statues[i];
+//         }
+//         if (statues[i] < smallest) {
+//             smallest = statues[i];
+//         }
+//     }
+//
+//     return ((largest - smallest) + 1) - statues.length;
+// }
+//
+// console.log(makeArrayConsecutive2([5, 2, 8, 4, 14]));
