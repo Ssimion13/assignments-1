ddf21ee098d750808353641584dd583920f12721
Ryan <rysherrell@gmail.com>
2018-03-05 18:14:37 -0700

    same

--- exercises/react-space/moms-shopping-list/src/Item.js
+++ exercises/react-space/moms-shopping-list/src/Item.js
@@ -3,6 +3,8 @@ import React from 'react';
 function Item(props) {
     return (
         <li>
+            <p style={props.completed}
+                key={}
             <button className="itemDoneButton"
                     onClick={() => props.handleItemDone(props.entry)}>
                 X
